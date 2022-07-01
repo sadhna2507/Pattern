5b17b652913849b6c8278e43cae442127a3f486c
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-07-01 16:35:59 +0530

    pattern

---
+++ index.js
@@ -0,0 +1,62 @@
+// let num = 3;
+// let px = num;
+// let py = num;
+// for(let i = 1; i<3; i++){
+//   stars = ``
+//   for(let j = 1; j<=3*2; j++){
+//     if(j==px||j==py){
+//        stars += "  * "
+//     }else stars +="    "
+   
+//   }px++
+//   py--
+//   console.log(stars)
+// }
+
+
+// px = 1;
+// py = num*2-1;
+// for(let i = 1; i<=3; i++){
+//   stars = ``
+//   for(let j = 1; j<=3*2; j++){
+//     if(j==px||j==py){
+//        stars += "  * "
+//     }else stars +="    "
+   
+//   }px++
+//   py--
+//   console.log(stars)
+// }
+
+
+
+
+// let num = 5;
+
+// for(let i = 1; i<=5; i++){
+//   let stars = ``
+//   for(let j = 1; j<=5; j++){
+//   if(i==j||(i+j==6)){
+//      stars += "   *    "
+//   }
+//   else{
+//     stars += "       "
+//   }
+  
+  
+//   }console.log(stars)
+// }
+
+
+// let num = 5;
+// let count = 1;
+// for(i=1; i<=num; i++){
+//     let stars = ''
+//     for(j=1; j<=i; j++){
+//         stars = stars + `${count}`;
+//         count = count+1;
+//     }
+//     console.log(stars);
+// }
+
+
